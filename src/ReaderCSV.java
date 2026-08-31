226c39fa111833281a265449fff1a18706724f19
Ryles <ryles.atm@gmail.com>
2026-08-31 23:18:15 +0200

    feat: initialisation du projet et structure

---
+++ src/ReaderCSV.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReaderCSV implements Reader {
+
+}
---
+++ src/WriterCSV.java
@@ -0,0 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WriterCSV implements Writer {
+
+}
