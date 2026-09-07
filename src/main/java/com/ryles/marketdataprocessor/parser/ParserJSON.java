95a60b5e0d557b867ee10dbfb1310b06079b43cd
Ryles <ryles.atm@gmail.com>
2026-09-07 08:39:39 +0200

    Ajouts du parsing() en JSON

--- src/main/java/com/ryles/marketdataprocessor/parser/ParserJSON.java
+++ src/main/java/com/ryles/marketdataprocessor/parser/ParserJSON.java
@@ -2,14 +2,69 @@ package com.ryles.marketdataprocessor.parser;
 
 import com.ryles.marketdataprocessor.model.MarketData;
 
+import java.math.BigDecimal;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ParserJSON implements Parser {
+    private Map<Path, List<String>> resultats;
 
-    public Map<Path,List<MarketData>> parsing() {
-        return null;
+    public ParserJSON(Map<Path,List<String>> resultats) {
+        this.resultats = resultats;
     }
 
+    public Map<Path,List<MarketData>> parsing() {
+        Map<Path,List<MarketData>> objets = new HashMap<>();
+        for (Map.Entry<Path, List<String>> var : this.resultats.entrySet()) {
+            StringBuffer sb = new StringBuffer();
+            StringBuffer stack = new StringBuffer();
+            List<MarketData> mD = new ArrayList<>();
+
+            // For each qui sert à uniformiser les formats JSON pour que tout soit sur une seule ligne
+            for (String foo : var.getValue()) {
+                sb.append(foo);
+            }
+
+            int longueur = sb.length();
+            boolean flag = false;
+
+            // Boucle qui sert à stack toutes les lignes entre { et } et qui instancie MarketData
+            for (int i=0 ; i<longueur ; i++) {
+                if (sb.charAt(i)=='{') {
+                    flag = true;
+                    continue;
+                }
+
+                if (flag && sb.charAt(i)!='}') {
+                    stack.append(sb.charAt(i));
+                }
+
+                if (flag && sb.charAt(i)=='}') {
+                    String[] chaine = stack.toString().split(",");
+
+                    int valDate = chaine[0].indexOf(":");
+                    int valSymbole = chaine[1].indexOf(":");
+                    int valPrix = chaine[2].indexOf(":");
+                    int valVolume = chaine[3].indexOf(":");
+
+                    LocalDateTime date = LocalDateTime.parse(chaine[0].substring(valDate+3,chaine[0].length()-1));
+                    String symbole = chaine[1].substring(valSymbole+3,chaine[1].length()-1);
+                    BigDecimal prix = new BigDecimal(chaine[2].substring(valPrix+2));
+                    long volume = Long.parseLong(chaine[3].substring(valVolume+2));
+
+                    mD.add(new MarketData(symbole,date,prix,volume));
+
+                    flag = false;
+                    stack.setLength(0);
+                }
+            }
+
+            objets.put(var.getKey(),mD);
+        }
+        return objets;
+    }
 }
